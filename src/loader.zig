5fc47692d029518069d63ae141511230bf47b739
Andy Armstrong <andy@hexten.net>
2025-10-23 18:13:28 +0100

    Minor

--- src/loader.zig
+++ src/loader.zig
@@ -2,7 +2,7 @@ pub const LoaderError = error{
     TypeMismatch,
     ArraySizeMismatch,
     MissingField,
-    BadEnum,
+    UnknownEnum,
     TupleSizeMismatch,
 };
 
@@ -252,7 +252,7 @@ pub fn Loader(comptime T: type) type {
                             },
                         };
                         if (tag) |t| return @enumFromInt(t);
-                        return LoaderError.BadEnum;
+                        return LoaderError.UnknownEnum;
                     }
                 };
             },
